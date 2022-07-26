0f0030acba923a51870241540dd87947cfc4c8ff
Rodrigo Ezequiel Miliano <rodrigomiliano16@gmail.com>
2022-07-25 22:57:01 -0300

    primer comimit

---
+++ src/main/java/ar/edu/davinci/dvds20211cg9/DvDs20211cG9Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20211cg9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20211cG9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20211cG9Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20211cg9/DvDs20211cG9ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20211cg9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20211cG9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
